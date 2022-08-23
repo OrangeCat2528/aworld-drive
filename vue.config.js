02a86ead45e870648cdd6cf889d084cadba634a6
OrangeCat <75605629+OrangeCat2528@users.noreply.github.com>
2022-08-23 20:57:53 +0700

    Update Config to HTTPS

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,21 @@
+// Customized for Aworld System
+
 const path = require('path')
+const fs = require('fs')
+
 module.exports = {
   outputDir: path.resolve(__dirname, './server/src/app/'),
   pwa: {
-    name: 'Aworld Manager',
+    name: 'Aworld Drive',
     themeColor: '#9013FE',
     msTileColor: '#50E3C2'
   },
   devServer: {
-    proxy: "http://localhost:3344"
+    https: {
+    key: fs.readFileSync('./sertifikat/drive.journeys.my.id+5-key.pem'),
+    cert: fs.readFileSync('./sertifikat/drive.journeys.my.id+5.pem'),
+    },
+    proxy: "https://localhost:443"
   },
   pluginOptions: {
     electronBuilder: {
